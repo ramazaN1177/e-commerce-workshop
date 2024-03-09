3c3148e3c50ee67be2d35c6feb609b1b225d3248
o_gkmn <ozgur.gokmen735@gmail.com>
2024-03-09 20:38:24 +0300

    variable is added

--- WebAPI/WeatherForecast.cs
+++ WebAPI/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace WebAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public int TryOut { get; set; }
     }
 }
